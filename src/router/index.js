35ba6955a70285f9a0ce4f3343cf574ee8772277
Ujjawal Rauniyar <ujjawalrauniyar2004@gmail.com>
2025-07-24 00:33:31 +0530

    index.js bug fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter , createWebHistory } from 'vue-router'
+import { createRouter , createWebHashHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import Dash from '../components/UserDashboard.vue'
 import Dash2 from '../components/AdminSubject.vue'
@@ -75,7 +75,7 @@ const routes =[
 ]
 
 const router = createRouter ({
-    history : createWebHistory(process.env.BASE_URL),
+    history : createWebHashHistory(process.env.BASE_URL),
     routes
 
 })
